8cf9d4ed9901e72a90c0e6e3776d3b6aa2e9f011
Ahnder <skguss1@gmail.com>
2020-06-16 13:10:40 +0900

    first commit, Add Component(TodoTemplate)

---
+++ src/components/TodoTemplate.jsx
@@ -0,0 +1,30 @@
+/**
+ *  todolist 의 레이아웃을 설정
+ */
+
+import React from 'react';
+import styled from 'styled-components';
+
+const TodoTemplateBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  width: 512px;
+  height: 768px;
+
+  margin: 0 auto;
+  margin-top: 96px;
+  margin-bottom: 32px;
+
+  background: #ffffff;
+
+  border-radius: 1rem;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+`;
+
+function TodoTemplate({ children }) {
+  return <TodoTemplateBlock>{children}</TodoTemplateBlock>;
+}
+
+export default TodoTemplate;
