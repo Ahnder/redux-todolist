ba4dae428afaae673fc3e71364af6ae4028e18b6
Ahnder <skguss1@gmail.com>
2020-06-16 17:18:33 +0900

    Add Modules(todos) & init Set redux(actiontype, func, intialState, reducer) & init Set rootReducer(/modules/index)

---
+++ src/modules/todos.js
@@ -0,0 +1,83 @@
+const CHANGE_INPUT = 'todos/CHANGE_INPUT';
+const INSERT = 'todos/INSERT';
+const TOGGLE = 'todos/TOGGLE';
+const REMOVE = 'todos/REMOVE';
+
+export const changeInput = (input) => ({
+  type: CHANGE_INPUT,
+  input,
+});
+let id = 5;
+export const insert = (text) => ({
+  type: INSERT,
+  todo: {
+    id: id++,
+    text,
+    done: false,
+  },
+});
+export const toggle = (id) => ({
+  type: TOGGLE,
+  id,
+});
+export const remove = (id) => ({
+  type: REMOVE,
+  id,
+});
+
+const initialState = {
+  input: '',
+  todos: [
+    {
+      id: 1,
+      text: '프로젝트 생성하기',
+      done: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링하기',
+      done: true,
+    },
+    {
+      id: 3,
+      text: 'Redux 적용하기',
+      done: false,
+    },
+    {
+      id: 4,
+      text: '기능 구현하기',
+      done: false,
+    },
+  ],
+};
+
+function todos(state = initialState, action) {
+  switch (action.type) {
+    case CHANGE_INPUT:
+      return {
+        ...state,
+        input: action.input,
+      };
+    case INSERT:
+      return {
+        ...state,
+        todos: state.todos.concat(action.todo),
+      };
+    case TOGGLE:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.id ? { ...todo, done: !todo.done } : todo,
+        ),
+      };
+    case REMOVE:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.id),
+      };
+    default:
+      throw new Error(`Unhandled action type : ${action.type}`);
+  }
+}
+
+export default todos;
