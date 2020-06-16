412ad0a129d925add30ccb7b138904fe78a807ce
Ahnder <skguss1@gmail.com>
2020-06-16 13:36:13 +0900

    Add Component(TodoList) & init Set CSS(TodoLIst)

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const TodoListBlock = styled.div``;
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 1.25rem 2rem;
+  padding-bottom: 3rem;
+  overflow-y: auto;
+`;
 
 function TodoList() {
   return (
