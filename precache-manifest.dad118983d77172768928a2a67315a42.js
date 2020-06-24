0de2869a87af5a317fdacdadff6b535f8f844a98
Ahnder <skguss1@gmail.com>
2020-06-24 14:43:25 +0900

    Updates

---
+++ precache-manifest.dad118983d77172768928a2a67315a42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf1679ae481e6f11540af0968903f0a",
+    "url": "/redux-todolist/index.html"
+  },
+  {
+    "revision": "e46fe69f1fa4b7306d32",
+    "url": "/redux-todolist/static/js/2.58bd8f70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-todolist/static/js/2.58bd8f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a755ba2b7993c578cb4",
+    "url": "/redux-todolist/static/js/main.05d24151.chunk.js"
+  },
+  {
+    "revision": "384e23ce2a0d4445a2b7",
+    "url": "/redux-todolist/static/js/runtime-main.1215448a.js"
+  }
+]);
